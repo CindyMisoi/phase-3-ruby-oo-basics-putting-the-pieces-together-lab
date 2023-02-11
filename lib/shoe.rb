fa2c0c35348aa38f9d3c7f939921f4a6a2f0b9d5
CindyMisoi <crmisoi2k@gmail.com>
2023-02-11 22:36:46 +0300

    completed assignment

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,14 @@
-# Make your shoe class here!
+class Shoe
+    # setter => macro
+    attr_writer :color, :size, :material, :condition
+    # getter => macro
+    attr_reader :brand, :color, :size, :material, :condition
+    def initialize(brand)
+        @brand = brand
+    end
+    def cobble
+        @condition = "new"
+        puts "Your shoe is as good as new!"
+        
+    end
+end
